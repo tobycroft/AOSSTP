f835079d760def51853851897aabfe3e4b1c5287
火线兔 <silverhawk@foxmail.com>
2022-05-14 20:36:48 +0800

    TuuzPC

--- extend/BaseController/CommonController.php
+++ extend/BaseController/CommonController.php
@@ -9,7 +9,7 @@ class CommonController extends Controller
     public function initialize()
     {
         header("Access-Control-Allow-Origin: *", true);
-//        header("access-control-allow-origin: *", true);
+        header("Access-Control-Max-Age: 86400", true);
         header("Access-Control-Allow-Credentials: true", true);
         header("Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE", true);
         header("Access-Control-Allow-Headers: *", true);
