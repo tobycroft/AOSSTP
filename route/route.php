524d664cb7b4676cc9067172ee3f9f8417bba27d
火线兔 <silverhawk@foxmail.com>
2022-05-14 10:46:27 +0800

    TuuzPC

--- route/route.php
+++ route/route.php
@@ -13,8 +13,17 @@ Route::get('think', function () {
     return 'hello,ThinkPHP5!';
 });
 
-Route::get('hello/:name', 'index/hello');
 
-return [
+Route::any(':version/:module/:controller/:function', '\app\:version\:module\controller\:controller@:function');
 
-];
+
+Route::any('up', '\app\v1\file\controller\index@up');
+Route::any('upfull', '\app\v1\file\controller\index@upfull');
+
+Route::any(':any', function () {
+    return \think\facade\Request::url();
+});
+
+Route::any('', function () {
+    return \think\facade\Request::url();
+});
