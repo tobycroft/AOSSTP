dae9596c622b08304e721e58a5bc14edaa02b54b
火线兔 <silverhawk@foxmail.com>
2022-09-05 12:35:55 +0800

    TuuzMac

--- application/v1/wechat/controller/index.php
+++ application/v1/wechat/controller/index.php
@@ -5,6 +5,7 @@ namespace app\v1\wechat\controller;
 use app\v1\wechat\model\WechatModel;
 use BaseController\CommonController;
 use WeMini\Crypt;
+use Yingou\MiniProgram\MiniProgram;
 
 class index extends CommonController
 {
@@ -25,10 +26,8 @@ class index extends CommonController
             \Ret::fail("未找到项目");
         }
         $this->config = [
-            'appid' => $wechat["app_id"],
-            'appsecret' => $wechat["secret"],
-            'token' => $wechat["token"],
-            'encodingaeskey' => $wechat["aes_key"]
+            'appId' => $wechat["app_id"],
+            'secret' => $wechat["secret"],
         ];
 
     }
@@ -36,13 +35,7 @@ class index extends CommonController
     public function qrcode()
     {
         $data = input('get.data');
-        $this->app = Crypt::instance();
-        $ret = $this->app->getClient()->postJson("wxa/getwxacodeunlimit", [
-            'scene' => '123',
-            'page' => 'pages/index/index',
-            'width' => 430,
-            'check_path' => false,
-        ]);
-        $path = $ret->saveAs();
+        $prog = new MiniProgram($this->config);
+        $prog->createQrCode->create("test", 480);
     }
 }
