84e7c7ac84a711e7ed4ef85afef4c638452d5275
火线兔 <silverhawk@foxmail.com>
2022-09-05 13:05:34 +0800

    TuuzMac

--- application/v1/wechat/controller/index.php
+++ application/v1/wechat/controller/index.php
@@ -37,7 +37,7 @@ class index extends CommonController
         $data = input('get.data');
         $prog = new MiniProgram(new ProgramConfig($this->config));
         $ret = $prog->createQrCode->create("/test?", 480);
-        imagejpeg($ret);
+        var_dump($ret);
     }
 }
 
