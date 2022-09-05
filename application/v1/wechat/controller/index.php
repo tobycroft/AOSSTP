8b457ce6c679ca6ca9a32fa10600e570b5637a35
火线兔 <silverhawk@foxmail.com>
2022-09-05 11:47:16 +0800

    TuuzMac

--- application/v1/wechat/controller/index.php
+++ application/v1/wechat/controller/index.php
@@ -2,10 +2,16 @@
 
 namespace app\v1\wechat\controller;
 
+use app\v1\wechat\model\WechatModel;
 use BaseController\CommonController;
+use EasyWeChat\MiniApp\Application;
 
 class index extends CommonController
 {
+
+    public $app;
+    public string $token;
+    public array $config;
 
     public function initialize()
     {
@@ -14,10 +20,29 @@ class index extends CommonController
         if (!$this->token) {
             \Ret::fail('token');
         }
+        $wechat = WechatModel::where("project", $this->token)->find();
+        if (!$wechat) {
+            \Ret::fail("未找到项目");
+        }
+        $this->config = [
+            'app_id' => $wechat["app_id"],
+            'secret' => $wechat["secret"],
+            'token' => $wechat["token"],
+            'aes_key' => $wechat["aes_key"]
+        ];
+
     }
 
-    public function index()
+    public function qrcode()
     {
-
+        $data = input('get.data');
+        $this->app = new Application($this->config);
+        $ret = $this->app->getClient()->postJson("wxa/getwxacodeunlimit", [
+            'scene' => '123',
+            'page' => 'pages/index/index',
+            'width' => 430,
+            'check_path' => false,
+        ]);
+        $path = $ret->saveAs();
     }
 }
