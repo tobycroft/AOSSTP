6c3b17dce212e60cfa8b4ce5e98a23e7ecae2b84
火线兔 <silverhawk@foxmail.com>
2022-09-22 14:02:11 +0800

    TuuzMac

--- extend/BaseController/CommonController.php
+++ extend/BaseController/CommonController.php
@@ -11,7 +11,7 @@ class CommonController extends Controller
         header("Access-Control-Allow-Origin: *", true);
         header("Access-Control-Max-Age: 86400", true);
         header("Access-Control-Allow-Credentials: false", true);
-        header("Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE", true);
+        header("Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS", true);
         header("Access-Control-Allow-Headers: *", true);
         // 服务启动
     }
