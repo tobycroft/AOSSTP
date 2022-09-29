621631f66c1e300a73861e41e8589a6c36ffb814
火线兔 <silverhawk@foxmail.com>
2022-09-29 18:31:51 +0800

    TuuzMac

--- application/v1/image/controller/create.php
+++ application/v1/image/controller/create.php
@@ -12,6 +12,7 @@ class create extends CommonController
 
     public function index(Request $request)
     {
+        header("Content-Type: image/jpg");
         $a = new InfoCodePhoto();
         $a->generate_photo("title", [], './upload/qr', './upload/qr');
     }
