41c6e5f78f77f4d4c79d829a5b7369c1c55ea296
火线兔 <silverhawk@foxmail.com>
2022-09-29 18:12:28 +0800

    TuuzMac

--- extend/CreateImg/InfoCodePhoto.php
+++ extend/CreateImg/InfoCodePhoto.php
@@ -134,8 +134,10 @@ class InfoCodePhoto
 
     public function deal_logo($im, $data)
     {
-        list($code_w, $code_h) = getimagesize($data["logo_path"]);
-        $codeImg = $this->createImageFromFile($data["logo_path"]);
+        $img = $this->getNetworkImgType($data["logo_path"]);
+//        list($code_w, $code_h) = getimagesize($data["logo_path"]);
+
+        $codeImg = $this->createImageFromFile($img);
         if (!is_null($codeImg)) {
             imagecopyresized($im, $codeImg, $data["logo_position_x"], $data["logo_position_y"], 0, 0, $data["logo_width"], $data["logo_height"], $code_w, $code_h);
         }
