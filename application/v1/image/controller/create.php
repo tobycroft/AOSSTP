3c9abce8e282c025fe43ec74f84a143b7c881ca9
火线兔 <silverhawk@foxmail.com>
2022-09-30 18:28:37 +0800

    TuuzMac

--- application/v1/image/controller/create.php
+++ application/v1/image/controller/create.php
@@ -108,7 +108,7 @@ class create extends CommonController
 //        $info = $img->save("../upload/image/" . $this->token . DIRECTORY_SEPARATOR . $md5 . ".jpg");
 //        $sav = $this->proc['url'] . '/' . "upload/image/" . $this->token . DIRECTORY_SEPARATOR . $md5 . ".jpg";
 //        \Ret::succ($sav);
-        $_FILES["file"]["name"] = "../public/upload/image/" . $this->token . "/" . $md5 . ".jpeg";
+        $_FILES["file"]["name"] = $md5 . ".jpeg";
         $_FILES["file"]["error"] = 0;
         $_FILES["file"]["type"] = "image/jpeg";
         $_FILES["file"]["tmp_name"] = "../public/upload/image/" . $this->token . "/" . $md5 . ".jpeg";
