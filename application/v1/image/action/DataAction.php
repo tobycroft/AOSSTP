8e7b50560c2060bf3e55a2ef93e6193884cbc073
火线兔 <silverhawk@foxmail.com>
2022-09-30 15:49:38 +0800

    `TuuzMac`

--- application/v1/image/action/DataAction.php
+++ application/v1/image/action/DataAction.php
@@ -9,36 +9,30 @@ class DataAction
         switch ($item["type"]) {
             case "text":
                 $layer = new Layer\Layer();
-                if (!isset($item["type"])) {
-                    $layer->type = $item["type"];
-                }
                 if (!isset($item["text"])) {
-                    $layer->type = $item["text"];
+                    $layer->text = $item["text"];
                 }
                 if (!isset($item["size"])) {
-                    $layer->type = $item["size"];
+                    $layer->size = $item["size"];
                 }
                 if (!isset($item["x"])) {
-                    $layer->type = $item["x"];
+                    $layer->x = $item["x"];
                 }
                 if (!isset($item["y"])) {
-                    $layer->type = $item["y"];
+                    $layer->y = $item["y"];
                 }
                 return $layer->text();
 
             case "image":
                 $layer = new Layer\Layer();
-                if (!isset($item["type"])) {
-                    $layer->type = $item["type"];
-                }
                 if (!isset($item["path"])) {
                     $layer->path = $item["path"];
                 }
                 if (!isset($item["x"])) {
-                    $layer->type = $item["x"];
+                    $layer->x = $item["x"];
                 }
                 if (!isset($item["y"])) {
-                    $layer->type = $item["y"];
+                    $layer->y = $item["y"];
                 }
                 return $layer->image();
         }
