3b85e4aa66a2f670f57a25422c8e0694c7be285d
火线兔 <silverhawk@foxmail.com>
2022-09-30 11:29:13 +0800

    `TuuzMac`

--- application/v1/image/controller/create.php
+++ application/v1/image/controller/create.php
@@ -11,15 +11,16 @@ class create extends CommonController
 
     public function initialize()
     {
-        header("Content-type:image/png");
+        header("Content-type:image/png", true);
 
     }
 
     public function index()
     {
+        header("Content-type:image/png", true);
         $img = new img("test");
         $img->create(200, 200);
-        header("Content-type:image/png");
+        header("Content-type:image/png", true);
     }
 
 }
