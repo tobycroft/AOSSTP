5b5cd8bbefba48e9da5f64d1350bab002281271a
火线兔 <silverhawk@foxmail.com>
2022-09-30 12:55:10 +0800

    `TuuzMac`

--- application/v1/image/controller/create.php
+++ application/v1/image/controller/create.php
@@ -17,8 +17,11 @@ class create extends CommonController
     public function index()
     {
         $img = new Canvas();
-        $img->create_empty_image(200, 400, "png");
-        $img->show();
+        $img->create_empty_image(200, 400, "png")
+            ->set_rgb('#df0d32')
+            ->merge("test_image.png", array("right", "bottom"))
+            ->filter("blur", 23)
+            ->show();
 //        \think\facade\Response::contentType("image/jpeg")->send();
     }
 
