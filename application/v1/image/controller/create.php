27cc0b9958a49017ca1a4ac62178881c7aebbbca
火线兔 <silverhawk@foxmail.com>
2022-09-30 18:42:34 +0800

    TuuzMac

--- application/v1/image/controller/create.php
+++ application/v1/image/controller/create.php
@@ -108,11 +108,12 @@ class create extends CommonController
 //        $info = $img->save("../upload/image/" . $this->token . DIRECTORY_SEPARATOR . $md5 . ".jpg");
 //        $sav = $this->proc['url'] . '/' . "upload/image/" . $this->token . DIRECTORY_SEPARATOR . $md5 . ".jpg";
 //        \Ret::succ($sav);
-        $_FILES["file"]["name"] = $md5 . ".jpg";
-        $_FILES["file"]["error"] = 0;
-        $_FILES["file"]["type"] = "image/png";
-        $_FILES["file"]["size"] = filesize("../public/upload/image/" . $this->token . "/" . $md5 . ".jpg");
-        $_FILES["file"]["tmp_name"] = "../public/upload/image/" . $this->token . "/" . $md5 . ".jpg";
+//        $_FILES["file"]["name"] = $md5 . ".jpg";
+//        $_FILES["file"]["error"] = 0;
+//        $_FILES["file"]["type"] = "image/png";
+//        $_FILES["file"]["size"] = filesize("../public/upload/image/" . $this->token . "/" . $md5 . ".jpg");
+//        $_FILES["file"]["tmp_name"] = "../public/upload/image/" . $this->token . "/" . $md5 . ".jpg";
+        $_POST["file"] = $image;
         $index = new index();
         $index->upload_file($request, 1, "complete");
     }
