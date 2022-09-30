99435d49ee1c6b6afbcf2067df5a3e22d2898d80
火线兔 <silverhawk@foxmail.com>
2022-09-30 11:28:12 +0800

    `TuuzMac`

--- application/v1/image/controller/create.php
+++ application/v1/image/controller/create.php
@@ -3,14 +3,20 @@
 namespace app\v1\image\controller;
 
 
+use BaseController\CommonController;
 use sezaicetin\Create\img;
 
-class create
+class create extends CommonController
 {
+
+    public function initialize()
+    {
+        header("Content-type:image/png");
+
+    }
 
     public function index()
     {
-        header("Content-type:image/png");
         $img = new img("test");
         $img->create(200, 200, "../public/img");
         header("Content-type:image/png");
