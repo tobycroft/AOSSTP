425e49e3911e3855619e585ffaf7146a8de77ec1
火线兔 <silverhawk@foxmail.com>
2022-09-30 11:33:06 +0800

    `TuuzMac`

--- config/app.php
+++ config/app.php
@@ -29,17 +29,17 @@ return [
     // 注册的根命名空间
     'root_namespace' => [],
     // 默认输出类型
-    'default_return_type' => 'html',
+    'default_return_type' => 'json',
     // 默认AJAX 数据返回格式,可选json xml ...
-    'default_ajax_return'    => 'json',
+    'default_ajax_return' => 'json',
     // 默认JSONP格式返回的处理方法
-    'default_jsonp_handler'  => 'jsonpReturn',
+    'default_jsonp_handler' => 'jsonpReturn',
     // 默认JSONP处理方法
-    'var_jsonp_handler'      => 'callback',
+    'var_jsonp_handler' => 'callback',
     // 默认时区
-    'default_timezone'       => 'Asia/Shanghai',
+    'default_timezone' => 'Asia/Shanghai',
     // 是否开启多语言
-    'lang_switch_on'         => false,
+    'lang_switch_on' => false,
     // 默认全局过滤方法 用逗号分隔多个
     'default_filter'         => '',
     // 默认语言
