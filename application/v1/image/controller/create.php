c4eafaeb98d3d0e46c60319795cca2a6561107ec
火线兔 <silverhawk@foxmail.com>
2022-09-30 14:17:18 +0800

    `TuuzMac`

--- application/v1/image/controller/create.php
+++ application/v1/image/controller/create.php
@@ -28,7 +28,9 @@ class create extends CommonController
     {
         $document = ImageWorkshop::initVirginLayer(200, 400);
         $layer1 = ImageWorkshop::initTextLayer("123", $this->font, $this->font_size, $this->font_color);
+        $img = ImageWorkshop::initFromPath("https://static.familyeducation.org.cn/ps/20220927/d1831a5f5af38d56ee0f414ff849e8aa.png");
         $document->addLayer(1, $layer1, 10, 10);
+        $document->addLayer(1, $img, 30, 40);
         $image = $document->getResult("ffffff");
 
 //        header('Content-type: image/jpeg');
