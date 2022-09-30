49d49dd1b535879ffa772c431a41570207e3a229
火线兔 <silverhawk@foxmail.com>
2022-09-30 10:47:32 +0800

    `TuuzMac`

--- application/v1/image/controller/create.php
+++ application/v1/image/controller/create.php
@@ -3,19 +3,15 @@
 namespace app\v1\image\controller;
 
 
-use think\Request;
 
 class create
 {
 
-    public function index(Request $request)
+    public function index()
     {
 //        header("Content-Type: image/jpg");
         $img = \Intervention\Image\Facades\Image::canvas(800, 600);
         $img->response();
     }
 
-    public function create(Request $request)
-    {
-    }
 }
