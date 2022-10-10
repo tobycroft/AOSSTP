461116e2051ef23ed6b5c417eaa6f541e2a3bdde
火线兔 <silverhawk@foxmail.com>
2022-10-10 11:09:44 +0800

    TuuzMac

--- application/v1/image/controller/qr.php
+++ application/v1/image/controller/qr.php
@@ -37,7 +37,7 @@ class qr extends CommonController
         $json = input("data");
         $opt = new QROptions([
             'version' => 7,
-            'eccLevel' => QRCode::ECC_Q,
+            'eccLevel' => QRCode::ECC_M,
             'scale' => 10,
             'imageBase64' => false,
             'bgColor' => [200, 200, 200],
