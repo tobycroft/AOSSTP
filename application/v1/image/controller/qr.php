5f8bee5a3aee1106acf8203e06a441ee9e0da32a
火线兔 <silverhawk@foxmail.com>
2022-10-10 12:04:59 +0800

    TuuzMac

--- application/v1/image/controller/qr.php
+++ application/v1/image/controller/qr.php
@@ -4,7 +4,6 @@ namespace app\v1\image\controller;
 
 use app\v1\project\model\ProjectModel;
 use BaseController\CommonController;
-use chillerlan\QRCode\Output\QRImage;
 use chillerlan\QRCode\QRCode;
 use chillerlan\QRCode\QROptions;
 use think\Request;
@@ -107,61 +106,6 @@ class qr extends CommonController
         $im = imagecreatefromstring($im);
         imagepng($im);
         \think\facade\Response::contentType("image/png")->send();
-    }
-
-}
-
-class QRImageWithLogo extends QRImage
-{
-
-    /**
-     * @param string|null $file
-     * @param string|null $logo
-     *
-     * @return string
-     * @throws \chillerlan\QRCode\Output\QRCodeOutputException
-     */
-    public function dump(string $file = null, string $logo = null): string
-    {
-        // set returnResource to true to skip further processing for now
-        $this->options->returnResource = true;
-
-        // of course you could accept other formats too (such as resource or Imagick)
-        // i'm not checking for the file type either for simplicity reasons (assuming PNG)
-//        $logo = file_get_contents($logo);
-//        if (!is_file($logo) || !is_readable($logo)) {
-//            throw new QRCodeOutputException('invalid logo');
-//        }
-
-        // there's no need to save the result of dump() into $this->image here
-        parent::dump($file);
-        $im = file_get_contents($logo);
-        $im = imagecreatefromstring($im);
-        // get logo image size
-        $w = imagesx($im);
-        $h = imagesy($im);
-
-        // set new logo size, leave a border of 1 module (no proportional resize/centering)
-        $lw = ($this->options->logoSpaceWidth - 2) * $this->options->scale;
-        $lh = ($this->options->logoSpaceHeight - 2) * $this->options->scale;
-
-        // get the qrcode size
-        $ql = $this->matrix->size() * $this->options->scale;
-
-        // scale the logo and copy it over. done!
-        imagecopyresampled($this->image, $im, ($ql - $lw) / 2, ($ql - $lh) / 2, 0, 0, $lw, $lh, $w, $h);
-
-        $imageData = $this->dumpImage();
-
-        if ($file !== null) {
-            $this->saveToFile($imageData, $file);
-        }
-
-        if ($this->options->imageBase64) {
-            $imageData = $this->toBase64DataURI($imageData, 'image/' . $this->options->outputType);
-        }
-
-        return $imageData;
     }
 
 }
