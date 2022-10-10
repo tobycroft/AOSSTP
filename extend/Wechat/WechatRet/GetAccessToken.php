ebba7b6abe2cff453d30e49f1972d20bdc8407f8
火线兔 <silverhawk@foxmail.com>
2022-10-10 15:54:34 +0800

    TuuzMac

--- extend/Wechat/WechatRet/GetAccessToken.php
+++ extend/Wechat/WechatRet/GetAccessToken.php
@@ -6,5 +6,27 @@ use Yingou\MiniProgram\MiniProgram;
 
 class GetAccessToken extends MiniProgram
 {
+    public $access_token;
+    public $expires_in;
+    private $error;
 
+    public function __construct($json)
+    {
+        $data = json_decode($json, 1);
+        if (isset($data["errmsg"])) {
+            $this->error = $data["errmsg"];
+        } else {
+            $this->access_token = $data["access_token"];
+            $this->expires_in = $data["expires_in"];
+        }
+    }
+
+    public function isSuccess()
+    {
+        if ($this->error) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 }
