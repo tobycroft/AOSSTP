0e26160653e1b00da51e76920264f1c4d53d610b
火线兔 <silverhawk@foxmail.com>
2022-10-10 16:25:26 +0800

    TuuzMac

--- extend/Wechat/Miniprogram.php
+++ extend/Wechat/Miniprogram.php
@@ -12,8 +12,7 @@ class Miniprogram extends WechatUrl
     {
 
         return new GetAccessToken(
-            raw_post(
-                \http_build_url(self::$Base . self::$getAccessToken, [
+            raw_post(self::$Base . self::$getAccessToken . http_build_query([
                         "appid" => $appid,
                         "secret" => $secret,
                         "grant_type" => $grant_type,
