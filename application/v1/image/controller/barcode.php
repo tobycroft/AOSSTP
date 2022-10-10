ba62301c3f6f76567fe2ddba7b65f194af82fdbf
火线兔 <silverhawk@foxmail.com>
2022-10-10 10:56:59 +0800

    TuuzMac

--- application/v1/image/controller/barcode.php
+++ application/v1/image/controller/barcode.php
@@ -33,7 +33,7 @@ class barcode extends CommonController
         if (!$request->has("data")) {
             \Ret::fail("data");
         }
-        $json = $request->input("data");
+        $json = input("data");
         $generator = new bc\BarcodeGeneratorPNG();
         echo $generator->getBarcode($json, $generator::TYPE_CODE_128);
         \think\facade\Response::contentType("image/png")->send();
