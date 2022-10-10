114653b60ff0ce04a3c29572820559752abbf4e8
火线兔 <silverhawk@foxmail.com>
2022-10-10 16:12:50 +0800

    TuuzMac

--- extend/Wechat/Miniprogram.php
+++ extend/Wechat/Miniprogram.php
@@ -19,3 +19,15 @@ class Miniprogram extends WechatUrl
 
 
 }
+
+function raw_post($send_url, $postData)
+{
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $send_url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
+    $response = curl_exec($ch);
+    curl_close($ch);
+    return $response;
+}
