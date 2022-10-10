8a56d3edad3d1fef0a84b02c8dc8af0786790bcd
火线兔 <silverhawk@foxmail.com>
2022-10-10 13:03:11 +0800

    TuuzMac

--- application/v1/image/controller/qr.php
+++ application/v1/image/controller/qr.php
@@ -148,6 +148,7 @@ class QRImageWithLogo extends QRImage
 //        $this->image = $im;
         // scale the logo and copy it over. done!
 //        imagecopyresampled($this->image, $im, ($ql - $lw) / 2, ($ql - $lh) / 2, 0, 0, $lw, $lh, $w, $h);
+        $this->image = $im;
         imagecopymerge($this->image, $im, ($ql - $lw) / 2, ($ql - $lh) / 2, 0, 0, $lw, $lh, 75);
         $imageData = $this->dumpImage();
 
