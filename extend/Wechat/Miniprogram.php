c7a6df3fa58d3781a9a7a74bd79ed97653600fe1
火线兔 <silverhawk@foxmail.com>
2022-10-10 15:57:33 +0800

    TuuzMac

--- extend/Wechat/Miniprogram.php
+++ extend/Wechat/Miniprogram.php
@@ -2,16 +2,18 @@
 
 namespace Wechat;
 
+use function Wechat\WechatRet\raw_post;
+
 class GetAccessToken extends WechatUrl
 {
     protected static $Base = "https://api.weixin.qq.com";
 
     public function getAccessToken(string $appid, $secret, $grant_type = "client_credential")
     {
-        return raw_post(self::$Base . self::$getAccessToken, [
+        return new \Wechat\WechatRet\GetAccessToken(raw_post(self::$Base . self::$getAccessToken, [
             "appid" => $appid,
             "secret" => $secret,
             "grant_type" => $grant_type,
-        ]);
+        ]));
     }
 }
