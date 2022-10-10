16db35d1a1568f163df470040b2144a2292441e7
火线兔 <silverhawk@foxmail.com>
2022-10-10 10:45:42 +0800

    TuuzMac

---
+++ application/v1/image/controller/barcode.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace app\v1\image\controller;
+
+use app\v1\project\model\ProjectModel;
+use BaseController\CommonController;
+use Picqer\Barcode;
+use think\Request;
+
+class barcode extends CommonController
+{
+
+
+    public mixed $token;
+    public mixed $proc;
+
+    public function initialize()
+    {
+        set_time_limit(0);
+        parent::initialize();
+        $this->token = input('get.token');
+        if (!$this->token) {
+            \Ret::fail('token');
+        }
+        $this->proc = ProjectModel::api_find_token($this->token);
+        if (!$this->proc) {
+            \Ret::fail('项目不可用');
+        }
+    }
+
+    public function png(Request $request)
+    {
+        if (!$request->has("data")) {
+            \Ret::fail("data");
+        }
+        $json = $request->post("data");
+        $generator = new Barcode\BarcodeGeneratorPNG();
+        echo $generator->getBarcode($json, $generator::TYPE_CODE_128);
+//        \think\facade\Response::contentType("image/png")->send();
+    }
+
+
+}
