1d18d79b56e142b301f73bdf7a589e1c1e0ca26b
火线兔 <silverhawk@foxmail.com>
2022-10-10 11:39:47 +0800

    TuuzMac

--- application/v1/image/controller/qr.php
+++ application/v1/image/controller/qr.php
@@ -99,6 +99,7 @@ class qr extends CommonController
         $qr = new QRCode($opt);
         $qlogo = new QRImageWithLogo($opt, $qr->getMatrix($json));
 
+
         echo $qlogo->dump(null, $url);
         \think\facade\Response::contentType("image/png")->send();
     }
