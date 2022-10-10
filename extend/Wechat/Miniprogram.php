3042771c038531004b6bf842b2940e29ace01685
火线兔 <silverhawk@foxmail.com>
2022-10-10 17:11:10 +0800

    TuuzMac

--- extend/Wechat/Miniprogram.php
+++ extend/Wechat/Miniprogram.php
@@ -36,12 +36,14 @@ class Miniprogram extends WechatUrl
 
 function raw_post($send_url, $postData = [])
 {
-    $postData = http_build_query($postData);
+    $headers = array("Content-type: application/json;charset=UTF-8", "Accept: application/json", "Cache-Control: no-cache", "Pragma: no-cache");
+//    $postData = http_build_query($postData);
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $send_url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_POST, 1);
     curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
     $response = curl_exec($ch);
     curl_close($ch);
     return $response;
