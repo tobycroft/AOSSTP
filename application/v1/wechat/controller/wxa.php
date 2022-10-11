bc1ef888c32e99574f5281f8a13b522c7e6585ed
火线兔 <silverhawk@foxmail.com>
2022-10-11 14:54:51 +0800

    TuuzMac

--- application/v1/wechat/controller/wxa.php
+++ application/v1/wechat/controller/wxa.php
@@ -181,7 +181,7 @@ class wxa extends create
         if ($this->proc["type"] == "oss" || $this->proc["type"] == "all") {
             try {
                 $oss = new AliyunOSS($this->proc);
-                $ret = $oss->uploadFile($this->proc['bucket'], $md5 . ".png", $fileName);
+                $ret = $oss->uploadFile($this->proc['bucket'], $md5 . ".png", "/wechat/" . $this->token . DIRECTORY_SEPARATOR . $md5 . ".jpg");
             } catch (OssException $e) {
                 \Ret::fail($e->getMessage(), 200);
             }
