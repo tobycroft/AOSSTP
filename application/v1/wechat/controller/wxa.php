8d3e49d34ba777ed2391e81a6934b34044de023d
火线兔 <silverhawk@foxmail.com>
2022-10-12 16:11:46 +0800

    TuuzMac

--- application/v1/wechat/controller/wxa.php
+++ application/v1/wechat/controller/wxa.php
@@ -145,7 +145,7 @@ class wxa extends create
         }
         $data = input('data');
         $page = input("page");
-        $md5 = md5($data . '|' . $page);
+        $md5 = md5($data);
 
         $wechat_data = WechatDataModel::where("key", $md5)->where("page", $page)->find();
         if (!empty($wechat_data)) {
