88346d620a560f5246dcbdd89c67fa7011538878
火线兔 <silverhawk@foxmail.com>
2022-10-13 16:36:36 +0800

    TuuzMac

--- application/v1/excel/controller/index.php
+++ application/v1/excel/controller/index.php
@@ -83,6 +83,66 @@ class index extends CommonController
         echo json_encode($colums);
     }
 
+    public function dp(Request $request)
+    {
+        $token = $this->token;
+        $proc = ProjectModel::api_find_token($token);
+        if (!$proc) {
+            \Ret::fail('项目不可用');
+            return;
+        }
+        $file = $request->file("file");
+        if (!$file) {
+            \Ret::fail('file字段没有用文件提交');
+            return;
+        }
+        $hash = $file->hash('md5');
+        if (!Validate::fileExt($file, ["xls", "xlsx"])) {
+            \Ret::fail("ext not allow");
+            return;
+        }
+        if (!Validate::fileSize($file, (float)8192 * 1024)) {
+            \Ret::fail("size too big");
+            return;
+        }
+
+        $info = $file->move('./upload/excel/' . $this->token);
+        $reader = IOFactory::load($info->getPathname());
+        unlink($info->getPathname());
+        $datas = $reader->getActiveSheet()->toArray();
+        if (count($datas) < 2) {
+            \Ret::fail("表格长度不足");
+            return;
+        }
+        $value = [];
+        $i = 0;
+        $keys = [];
+        foreach ($datas[0] as $data) {
+            if (!empty($data)) {
+                $keys[] = $data;
+            }
+        }
+        foreach ($keys as $key) {
+            if (empty($key)) {
+                \Ret::fail("表格长度不一");
+                return;
+            }
+        }
+        $count_column = count($keys);
+        $colums = [];
+        for ($i = 1; $i < count($datas); $i++) {
+            $line = $datas[$i];
+            if (empty($line[0])) {
+                continue;
+            }
+            for ($s = 0; $s < $count_column; $s++) {
+                $arr[$keys[$s]] = $line[$s] ?: "";
+            }
+            $colums[] = $arr;
+        }
+        \Ret::succ(json_encode($colums));
+    }
+
     public function force(Request $request)
     {
         $token = $this->token;
