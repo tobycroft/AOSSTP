e9a7726f9f21df1ad04dac0a6b84d271aa7ae947
火线兔 <silverhawk@foxmail.com>
2022-10-19 11:07:26 +0800

    TuuzMac

--- application/v1/file/controller/sig.php
+++ application/v1/file/controller/sig.php
@@ -2,7 +2,29 @@
 
 namespace app\v1\file\controller;
 
+use app\v1\project\model\ProjectModel;
+use think\Request;
+
 class sig
 {
+    public $token;
+    public $proc;
+
+    public function initialize()
+    {
+        $this->token = input('get.token');
+        if (!$this->token) {
+            \Ret::Fail(401, null, 'token');
+        }
+        $this->proc = ProjectModel::api_find_token($this->token);
+        if (!$this->proc) {
+            \Ret::Fail(401, null, '项目不可用');
+        }
+    }
+
+    public function get(Request $request)
+    {
+        
+    }
 
 }
