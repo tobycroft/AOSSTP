f9ff36b64d674d7d63352ad453fdd66a17250cbb
火线兔 <silverhawk@foxmail.com>
2022-10-31 11:02:02 +0800

    TuuzPC

--- application/v1/excel/controller/search.php
+++ application/v1/excel/controller/search.php
@@ -29,9 +29,43 @@ class search extends index
         if ($this->proc["type"] == "all" && !file_exists('./upload/' . $file_info['path'])) {
             \Ret::Fail('404', null, '本地文件不存在');
         }
-        $file = file_get_contents("");
-        $info = $file->move('./upload/excel/' . $this->token);
-        $reader = IOFactory::load('./upload/' . $file_info['path']);
-        $this->extracted($reader);
+        $file = file_get_contents($this->proc["url"] . '/' . $file_info['path']);
+        $savname = './upload/excel/' . $this->token . DIRECTORY_SEPARATOR . $md5 . '.' . $file_info['ext'];
+        if (file_put_contents($savname, $file)) {
+            $reader = IOFactory::load($savname);
+            $datas = $reader->getActiveSheet()->toArray();
+            if (count($datas) < 2) {
+                \Ret::Fail(400, null, '表格长度不足');
+                return;
+            }
+            $value = [];
+            $i = 0;
+            $keys = [];
+            foreach ($datas[0] as $data) {
+                if (!empty($data)) {
+                    $keys[] = $data;
+                }
+            }
+            foreach ($keys as $key) {
+                if (empty($key)) {
+                    \Ret::Fail(400, null, '表格长度不一');
+                    return;
+                }
+            }
+            $count_column = count($keys);
+            $colums = [];
+            for ($i = 1; $i < count($datas); $i++) {
+                $line = $datas[$i];
+                if (empty($line[0])) {
+                    continue;
+                }
+                for ($s = 0; $s < $count_column; $s++) {
+                    $arr[$keys[$s]] = $line[$s] ?: '';
+                }
+                $colums[] = $arr;
+            }
+            \Ret::Success(0, $colums);
+        }
+
     }
 }
