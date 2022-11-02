324eb5e4bad2bd97bd032735d329d790732a95d1
火线兔 <silverhawk@foxmail.com>
2022-11-02 12:44:28 +0800

    TuuzPC

---
+++ application/v1/logger/model/LoggerDbrrModel.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace app\v1\logger\model;
+
+
+use think\Db;
+
+class LoggerDbrrModel extends Db
+{
+
+    public static $table = 'ao_logger_dbrr';
+
+
+}
