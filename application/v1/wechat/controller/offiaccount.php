d4d03c303343578c583e8ff2b495ed8f391dbccb
火线兔 <silverhawk@foxmail.com>
2022-11-10 18:17:41 +0800

    TuuzPC

--- application/v1/wechat/controller/offiaccount.php
+++ application/v1/wechat/controller/offiaccount.php
@@ -74,4 +74,15 @@ class offiaccount extends create
             \Ret::Fail(300, $wxa->response, $wxa->getError());
         }
     }
+
+    public function user_openid()
+    {
+        $appid = 'wx520c15f417810387';
+        $redirect_uri = 'https%3A%2F%2Fchong.qq.com%2Fphp%2Findex.php%3Fd%3D%26c%3DwxAdapter%26m%3DmobileDeal%26showwxpaytitle%3D1%26vb2ctag%3D4_2030_5_1194_60';
+        $response_type = 'code';
+        $scope = 'snsapi_base';
+        $state = '123';
+        $redirect_uri = urlencode($redirect_uri);
+        \Ret::Success(0, "https://open.weixin.qq.com/connect/oauth2/authorize?appid=$appid&redirect_uri=$redirect_uri&response_type=$response_type&scope=$scope&state=$state#wechat_redirect");
+    }
 }
