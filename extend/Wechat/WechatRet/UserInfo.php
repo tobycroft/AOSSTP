4c63405825fb113d9db60f5afddb7afd9b5bac8f
火线兔 <silverhawk@foxmail.com>
2022-11-10 18:02:22 +0800

    TuuzPC

--- extend/Wechat/WechatRet/UserInfo.php
+++ extend/Wechat/WechatRet/UserInfo.php
@@ -6,11 +6,44 @@ namespace Wechat\WechatRet;
 class UserInfo
 {
     public $response;
-    public mixed $session_key;
-    public mixed $unionid;
-    public mixed $openid;
+
     protected $data;
     protected mixed $error;
+
+    public int $subscribe;
+    public string $openid;
+    public string $nickname;
+    public int $sex;
+    public string $language;
+
+    public string $city;
+    public string $province;
+    public string $country;
+
+    public string $headimgurl;
+    public int $subscribe_time;
+    public string $subscribe_scene;
+
+    /*
+     * {
+    'subscribe': 1,
+    'openid': 'oWboX6gK1VcGcIj5jG_XKEQVx4Fc',
+    'nickname': '',
+    'sex': 0,
+    'language': 'zh_CN',
+    'city': '',
+    'province': '',
+    'country': '',
+    'headimgurl': '',
+    'subscribe_time': 1668071043,
+    'remark': '',
+    'groupid': 0,
+    'tagid_list': [],
+    'subscribe_scene': 'ADD_SCENE_QR_CODE',
+    'qr_scene': 0,
+    'qr_scene_str': ''
+}
+     */
 
     public function __construct($json)
     {
