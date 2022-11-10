4e29cf23fcfbd4a30e80a26b95c977c8bac0eb9f
火线兔 <silverhawk@foxmail.com>
2022-11-10 18:24:59 +0800

    TuuzPC

--- application/v1/wechat/controller/offiaccount.php
+++ application/v1/wechat/controller/offiaccount.php
@@ -79,7 +79,7 @@ class offiaccount extends create
     public function user_openid()
     {
         $appid = $this->appid;
-        $redirect_uri = 'https%3A%2F%2Fchong.qq.com%2Fphp%2Findex.php%3Fd%3D%26c%3DwxAdapter%26m%3DmobileDeal%26showwxpaytitle%3D1%26vb2ctag%3D4_2030_5_1194_60';
+        $redirect_uri = 'https://api.lc.familyeducation.org.cn';
         $response_type = 'code';
         $scope = 'snsapi_base';
         $state = '123';
