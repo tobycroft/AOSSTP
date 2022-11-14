d8919fe2098ffc79cef48d860a1413001727ed12
火线兔 <silverhawk@foxmail.com>
2022-11-14 11:26:20 +0800

    TuuzMac

--- extend/Wechat/OfficialAccount.php
+++ extend/Wechat/OfficialAccount.php
@@ -55,7 +55,7 @@ class OfficialAccount extends Miniprogram
 //                    'appid' => $appid,
                     'template_id' => $template_id,
                     'url' => $url,
-                    'data' => $data,
+                    'data' => json_decode($data, 1),
                 ],
             ]
         ));
