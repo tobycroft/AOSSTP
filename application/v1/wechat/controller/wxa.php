592b1514a265ff4f21a07ea88eeac19fb405de4b
火线兔 <silverhawk@foxmail.com>
2022-11-30 09:41:25 +0800

    TuuzMac

--- application/v1/wechat/controller/wxa.php
+++ application/v1/wechat/controller/wxa.php
@@ -146,7 +146,7 @@ class wxa extends create
         $wechat_data = WechatDataModel::where("key", $md5)->where("project", $this->token)->where("page", $page)->find();
         if (!empty($wechat_data)) {
             if (file_exists($this->path_prefix . $wechat_data["path"])) {
-                \Ret::Success(0, $this->proc['url'] . "/wechat/" . $this->token . DIRECTORY_SEPARATOR . $md5 . ".jpg", $wechat_data['path']);
+                \Ret::Success(0, $this->proc['url'] . $wechat_data["path"], "from_cache");
             }
         }
         $wxa = Miniprogram::getWxaCodeUnlimit($this->access_token, $md5, $page, 400);
