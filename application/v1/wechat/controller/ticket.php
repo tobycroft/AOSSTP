a4462eb9ee130452a932fe019a2777a3b770271f
火线兔 <silverhawk@foxmail.com>
2022-12-14 16:57:21 +0800

    TuuzMac

--- application/v1/wechat/controller/ticket.php
+++ application/v1/wechat/controller/ticket.php
@@ -2,7 +2,7 @@
 
 namespace app\v1\wechat\controller;
 
-use app\v1\wechat\action\AccessTokenAction;
+use app\v1\wechat\model\WechatModel;
 use think\Request;
 
 class ticket extends wxa
@@ -22,21 +22,31 @@ class ticket extends wxa
 
         $ticket = \Wechat\Ticket::getTicket($this->access_token);
         if ($ticket->isSuccess()) {
-            \Ret::Success(0, $ticket->getData());
+            WechatModel::where("project", $this->token)->update([
+                "ticket" => $ticket->ticket,
+                'ticket_expire_after' => $ticket->expires_in + time() - 600
+            ]);
+            \Ret::Success(0, $ticket->isSuccess());
         } else {
-            $this->ac->auto_error_code($ticket->getErrcode());
-            \Ret::Fail(300, $ticket->response, $ticket->getError());
+            \Ret::Fail(300, $ticket->response, $ticket->error());
         }
     }
 
     public function signature(Request $request)
     {
-        $expire_after = strtotime($wechat['expire_after']);
-        $this->ac = new AccessTokenAction($this->token, $this->appid, $this->appsecret);
-        if ($expire_after < time() || empty($this->access_token)) {
-            $this->ac->refresh_token();
-            $this->access_token = $this->ac->get_access_token();
-        }
+        $noncestr = input("noncestr");
+        $jsapi_ticket = input("jsapi_ticket");
+        $timestamp = input('timestamp');
+        $url = input('url');
+        $post = [
+            'noncestr' => $noncestr,
+            'jsapi_ticket' => $jsapi_ticket,
+            'timestamp' => $timestamp,
+            'url' => $url,
+        ];
+        ksort($post);
+        $str = http_build_query($post);
+        \Ret::Success(0, sha1($str));
     }
 
 }
