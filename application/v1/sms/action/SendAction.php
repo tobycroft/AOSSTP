0f6cab1f9321a553a69110f8ffac0e6c79d29f0d
火线兔 <silverhawk@foxmail.com>
2023-02-11 13:52:44 +0800

    TuuzPC

--- application/v1/sms/action/SendAction.php
+++ application/v1/sms/action/SendAction.php
@@ -12,7 +12,7 @@ class SendAction
             case "aliyun":
                 $aliyun = SmsAliyunModel::where("tag", $proc["sms_tag"])->findOrEmpty();
                 if ($aliyun) {
-                    var_dump(AliyunAction::Send($type, $tag, aliyun['accessid'], $aliyun['accesskey'], $aliyun['sign'], $aliyun['tpcode'], $phone, $param));
+                    var_dump(AliyunAction::Send($proc['sms_type'], $proc['sms_tag'], aliyun['accessid'], $aliyun['accesskey'], $aliyun['sign'], $aliyun['tpcode'], $phone, $param));
                 }
                 break;
 
