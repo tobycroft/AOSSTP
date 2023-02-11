13351422796cb07fae97149ab0067595705f9c2c
火线兔 <silverhawk@foxmail.com>
2023-02-11 13:25:12 +0800

    TuuzPC

--- extend/Input.php
+++ extend/Input.php
@@ -45,7 +45,7 @@ class Input
     {
         $in = Request::post($name);
         var_dump($in);
-        echo is_integer($in);
+        var_dump(is_integer($in));
         if (!$in && $must_have) {
             Ret::Fail(400, null, 'Input-Post-Int:[' . $name . ']');
             return 0;
