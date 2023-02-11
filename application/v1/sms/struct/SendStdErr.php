a0abbd5c3493cba9fddf1e25c9e30ecb63b9768b
火线兔 <silverhawk@foxmail.com>
2023-02-11 14:47:51 +0800

    TuuzPC

---
+++ application/v1/sms/struct/SendStdErr.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace app\v1\sms\struct;
+
+class SendStdErr
+{
+    public int $code;
+    public mixed $data = [];
+    protected string|null $error = null;
+
+    public function __construct(int $code, $data = [], $error = null)
+    {
+        $this->code = $code;
+        $this->data = $data;
+        $this->error = $error;
+    }
+
+    public function getData()
+    {
+        return $this->data;
+    }
+
+    public function isSuccess(): bool
+    {
+        if ($this->code === 0) {
+            return true;
+        }
+        return empty($this->error);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getError(): mixed
+    {
+        return $this->error;
+    }
+}
