16c78e1bf7ec57f91c03db5571a2f9cee140342b
火线兔 <silverhawk@foxmail.com>
2023-02-12 13:05:53 +0800

    TuuzPC

--- application/v1/hook/controller/push.php
+++ application/v1/hook/controller/push.php
@@ -10,7 +10,44 @@ class push
     public function single()
     {
         $tag = \Input::Get("tag");
-        $data = HookModel::where("tag", $tag)->find();
+        $data = HookModel::where("tag", $tag)->getData();
+        if ($data) {
+            $push = [];
+            foreach ($data as $datum) {
+                switch ($data['mode']) {
+                    case 'aapanel':
+                        $path = $data['method'] . '://' . $data['domain'] . '/hook';
+                        $query = [
+                            'access_key' => $data['key'],
+                        ];
+                        $ret = HookAction::raw_post($path, $query);
+                        if ($ret) {
+                            $push[$data["remark"]] = $ret;
+                        } else {
+                            \Ret::Fail(200, $ret);
+                        }
+                        break;
+
+                    default:
+                        $ret = HookAction::raw_post($data['url']);
+                        if ($ret) {
+                            $push[$data['remark']] = $ret;
+                        } else {
+                            \Ret::Fail(200, $ret);
+                        }
+                        break;
+                }
+                \Ret::Success(0, $push);
+            }
+        } else {
+            \Ret::Fail(404, null, "未找到项目");
+        }
+    }
+
+    public function github()
+    {
+        $tag = \Input::Get("tag");
+        $data = HookModel::where("tag", $tag)->getData();
         if ($data) {
             switch ($data["mode"]) {
                 case "aapanel":
