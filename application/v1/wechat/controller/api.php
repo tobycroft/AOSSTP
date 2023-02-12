0101945d4d14a3bc4a7bcfb64830488d8ee3fafb
火线兔 <silverhawk@foxmail.com>
2023-02-12 18:10:29 +0800

    TuuzPC

--- application/v1/wechat/controller/api.php
+++ application/v1/wechat/controller/api.php
@@ -2,14 +2,22 @@
 
 namespace app\v1\wechat\controller;
 
+use app\v1\file\controller\search;
 use app\v1\log\model\LogWebModel;
 use app\v1\wechat\model\WechatModel;
 
-class api
+class api extends search
 {
+
 
     public function recv()
     {
+        $project = \Input::Get('project');
+        $data = WechatModel::where('project', $project)->find();
+        if (!$data) {
+            \Ret::Fail(401, $project, '项目不可用');
+        }
+
         if (request()->isGet()) {
             $this->get();
         } else {
@@ -19,6 +27,7 @@ class api
 
     public function post()
     {
+        $in = \Input::Raw();
 
     }
 
@@ -34,6 +43,26 @@ class api
             'method' => request()->method(),
         ]);
 
+        $signature = \Input::Get('signature');
+        $timestamp = \Input::Get('timestamp');
+        $echostr = \Input::Get('echostr');
+        $nonce = \Input::Get('nonce');
+
+
+        $tmpArr = array($data['token'], $timestamp, $nonce);
+        sort($tmpArr, SORT_STRING);
+        $tmpStr = implode($tmpArr);
+        $tmpStr = sha1($tmpStr);
+
+        if ($tmpStr == $signature) {
+            echo $echostr;
+        } else {
+            echo 0;
+        }
+    }
+
+    public function verify()
+    {
         $project = \Input::Get('project');
         $signature = \Input::Get('signature');
         $timestamp = \Input::Get('timestamp');
