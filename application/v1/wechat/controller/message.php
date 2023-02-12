f532137d9e7b8dc144766ce36e3784c62d2036e0
火线兔 <silverhawk@foxmail.com>
2023-02-13 00:33:13 +0800

    TuuzPC

--- application/v1/wechat/controller/message.php
+++ application/v1/wechat/controller/message.php
@@ -2,6 +2,10 @@
 
 namespace app\v1\wechat\controller;
 
+use Input;
+use Ret;
+use Wechat\KefuMessage;
+
 class message extends info
 {
 
@@ -12,6 +16,15 @@ class message extends info
 
     public function send()
     {
-
+        $openid = Input::Post('openid');
+        $content = Input::Post('content');
+        $kefu = new KefuMessage($this->access_token, $openid);
+        $kefu->text($content);
+        $ret = $kefu->send();
+        if ($ret->isSuccess()) {
+            Ret::Success();
+        } else {
+            Ret::Fail(200, $ret->response, $ret->getError());
+        }
     }
 }
