19a32f3b26a6778a0126091018d5d3558fbb6e0e
火线兔 <silverhawk@foxmail.com>
2023-02-12 13:10:06 +0800

    TuuzPC

--- application/v1/hook/controller/push.php
+++ application/v1/hook/controller/push.php
@@ -47,36 +47,46 @@ class push
         }
     }
 
+    //http://upload.tuuz.cc:8000/v1/hook/push/github
     public function github()
     {
-        $tag = \Input::Get("tag");
-        $data = HookModel::where("tag", $tag)->getData();
+        echo input("repository");
+        return;
+        $tag = \Input::Get('tag');
+        $data = HookModel::where('tag', $tag)->getData();
         if ($data) {
-            switch ($data["mode"]) {
-                case "aapanel":
-                    $path = $data["method"] . "://" . $data["domain"] . "/hook";
-                    $query = [
-                        "access_key" => $data["key"],
-                    ];
-                    $ret = HookAction::raw_post($path, $query);
-                    if ($ret) {
-                        \Ret::Success(0, $ret);
-                    } else {
-                        \Ret::Fail(200, $ret);
-                    }
-                    break;
+            $rets = [];
+            $status = [];
+            foreach ($data as $datum) {
+                switch ($datum['mode']) {
+                    case 'aapanel':
+                        $path = $datum['method'] . '://' . $datum['domain'] . '/hook';
+                        $query = [
+                            'access_key' => $datum['key'],
+                        ];
+                        $ret = HookAction::raw_post($path, $query);
+                        $rets[$datum['remark']] = $ret;
+                        if ($ret) {
+                            $status[$datum['remark']] = 'success';
+                        } else {
+                            $status[$datum['remark']] = 'fail';
+                        }
+                        break;
 
-                default:
-                    $ret = HookAction::raw_post($data['url']);
-                    if ($ret) {
-                        \Ret::Success(0, $ret);
-                    } else {
-                        \Ret::Fail(200, $ret);
-                    }
-                    break;
+                    default:
+                        $ret = HookAction::raw_post($datum['url']);
+                        $rets[$datum['remark']] = $ret;
+                        if ($ret) {
+                            $status[$datum['remark']] = 'success';
+                        } else {
+                            $status[$datum['remark']] = 'fail';
+                        }
+                        break;
+                }
+                \Ret::Success(0, $rets);
             }
         } else {
-            \Ret::Fail(404, null, "未找到项目");
+            \Ret::Fail(404, null, '未找到项目');
         }
     }
 }
