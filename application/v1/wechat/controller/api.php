e6819cb494c339d097731c64e3ca0d074b634f83
火线兔 <silverhawk@foxmail.com>
2023-02-12 17:14:38 +0800

    TuuzPC

--- application/v1/wechat/controller/api.php
+++ application/v1/wechat/controller/api.php
@@ -3,6 +3,7 @@
 namespace app\v1\wechat\controller;
 
 use app\v1\log\model\LogWebModel;
+use app\v1\wechat\model\WechatModel;
 
 class api
 {
@@ -16,5 +17,26 @@ class api
             "raw" => json_encode(request()->getInput()),
             "header" => json_encode(request()->header()),
         ]);
+
+        $project = \Input::Get("project");
+        $signature = \Input::Get("signature");
+        $timestamp = \Input::Get('timestamp');
+        $nonce = \Input::Get('nonce');
+
+        $data = WechatModel::where("project", $project)->find();
+        if (!$data) {
+            \Ret::Fail(401, null, "项目不可用");
+        }
+
+        $tmpArr = array($data['token'], $timestamp, $nonce);
+        sort($tmpArr, SORT_STRING);
+        $tmpStr = implode($tmpArr);
+        $tmpStr = sha1($tmpStr);
+
+        if ($tmpStr == $signature) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
