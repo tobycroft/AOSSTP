e489289b7f6d77a0332f5967ebfeb85c4211468d
火线兔 <silverhawk@foxmail.com>
2023-02-12 15:31:19 +0800

    TuuzPC

--- application/v1/wechat/controller/api.php
+++ application/v1/wechat/controller/api.php
@@ -2,16 +2,15 @@
 
 namespace app\v1\wechat\controller;
 
-use app\v1\wechat\model\WechatMessage;
-
 class api
 {
 
     public static function recv()
     {
-        $in = \Input::Raw();
-        WechatMessage::create([
-            "raw" => $in
-        ]);
+        var_dump(request()->getInput());
+//        $in = \Input::Raw();
+//        WechatMessage::create([
+//            "raw" => $in
+//        ]);
     }
 }
