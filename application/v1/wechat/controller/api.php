4ac8520bfcca693afb9ce84457e08c3e4b41b359
火线兔 <silverhawk@foxmail.com>
2023-02-12 18:53:19 +0800

    TuuzPC

--- application/v1/wechat/controller/api.php
+++ application/v1/wechat/controller/api.php
@@ -55,7 +55,7 @@ class api extends search
 //        var_dump($data);
 //        var_dump($index);
 
-        $data = simplexml_load_string($xmltext, 'SimpleXMLElement', LIBXML_NOCDATA + LIBXML_NOBLANKS + LIBXML_NOBLANKS);
+        $data = simplexml_load_string($xmltext, 'SimpleXMLElement', LIBXML_NOCDATA + LIBXML_NOBLANKS + LIBXML_NOEMPTYTAG);
         echo json_encode($data, 320);
 
 //        var_dump($index);
