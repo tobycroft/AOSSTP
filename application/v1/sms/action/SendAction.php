c2eb7c0c26ccdcf0481bffc0fec5c2eac51c7719
火线兔 <silverhawk@foxmail.com>
2023-02-13 10:02:39 +0800

    TuuzPC

--- application/v1/sms/action/SendAction.php
+++ application/v1/sms/action/SendAction.php
@@ -6,6 +6,7 @@ use app\v1\sms\model\SmsAliyunModel;
 use app\v1\sms\model\SmsLcModel;
 use app\v1\sms\model\SmsTencentModel;
 use app\v1\sms\struct\SendStdErr;
+use Ret;
 
 class SendAction
 {
@@ -19,7 +20,7 @@ class SendAction
                 if ($data) {
                     return AliyunAction::Send($proc['sms_type'], $proc['sms_tag'], $data['accessid'], $data['accesskey'], $phone, $text, $data['sign'], $data['tpcode']);
                 }
-                \Ret::Fail(408, null, '未找到aliyun平台对应模板');
+                Ret::Fail(408, null, '未找到aliyun平台对应模板');
                 break;
 
             case "tencent":
@@ -27,31 +28,31 @@ class SendAction
                 if ($data) {
                     return TencentSmsAction::Send($proc['sms_type'], $proc['sms_tag'], $data['appid'], $data['appkey'], $quhao, $phone, $text, $data['sign'], $data['tplid']);
                 }
-                \Ret::Fail(408, null, '未找到tencent平台对应模板');
+                Ret::Fail(408, null, '未找到tencent平台对应模板');
                 break;
 
             case "ihuyi":
-                \Ret::Fail(408, null, '2');
+                Ret::Fail(408, null, '2');
                 break;
 
             case "zz253":
-                \Ret::Fail(408, null, '3');
+                Ret::Fail(408, null, '3');
                 break;
 
             case "lc":
                 $data = SmsLcModel::where('tag', $proc['sms_tag'])->findOrEmpty();
                 if ($data) {
-                    if (count($text) <= 6) {
+                    if (strlen($text) <= 6) {
                         return LcAction::SendCode($proc['sms_type'], $proc['sms_tag'], $data['reverse_addr'], $data['mch_id'], $data['key'], $phone, $text, $data['sign'], $data['tpcode']);
                     } else {
                         return LcAction::SendText($proc['sms_type'], $proc['sms_tag'], $data['reverse_addr'], $data['mch_id'], $data['key'], $phone, $text, $data['sign'], $data['tpcode']);
                     }
                 }
-                \Ret::Fail(408, null, '未找到LC平台对应模板');
+                Ret::Fail(408, null, '未找到LC平台对应模板');
                 break;
 
             default:
-                \Ret::Fail(408, null, "项目没有对应的短信方案或模板");
+                Ret::Fail(408, null, "项目没有对应的短信方案或模板");
                 break;
         }
         return null;
