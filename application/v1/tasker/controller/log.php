c9f4b8826baca25ac806fdc72e85241a08e0c4e5
火线兔 <silverhawk@foxmail.com>
2023-02-16 16:13:28 +0800

    TuuzMac

--- application/v1/tasker/controller/log.php
+++ application/v1/tasker/controller/log.php
@@ -2,10 +2,21 @@
 
 namespace app\v1\tasker\controller;
 
+use app\v1\log\model\LogWebModel;
+use Input;
+
 class log
 {
     public function upload()
     {
+        $in = Input::Raw();
+        LogWebModel::create([
+            'get' => json_encode(request()->get()),
+            'post' => json_encode(request()->post()),
+            'raw' => $in,
+            'header' => json_encode(request()->header()),
+            'method' => request()->method(),
+        ]);
         echo 123;
     }
 }
