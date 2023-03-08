dff3e05062bd38ff6fec8f96896eb6e8018269fc
火线兔 <silverhawk@foxmail.com>
2023-03-08 17:11:52 +0800

    TuuzMac

--- application/v1/lcic/controller/user.php
+++ application/v1/lcic/controller/user.php
@@ -71,15 +71,6 @@ class user extends create
         $OriginId = \Input::Post("OriginId");
         $Avatar = \Input::Post("Avatar");
         try {
-            // 实例化一个认证对象，入参需要传入腾讯云账户 SecretId 和 SecretKey，此处还需注意密钥对的保密
-            // 代码泄露可能会导致 SecretId 和 SecretKey 泄露，并威胁账号下所有资源的安全性。以下代码示例仅供参考，建议采用更安全的方式来使用密钥，请参见：https://cloud.tencent.com/document/product/1278/85305
-            // 密钥可前往官网控制台 https://console.cloud.tencent.com/cam/capi 进行获取
-            // 实例化一个http选项，可选的，没有特殊需求可以跳过
-
-            // 实例化一个client选项，可选的，没有特殊需求可以跳过
-            // 实例化要请求产品的client对象,clientProfile是可选的
-
-            // 实例化一个请求对象,每个接口都会对应一个request对象
             $req = new RegisterUserRequest();
 
             $params = array(
@@ -89,9 +80,7 @@ class user extends create
                 'Avatar' => $Avatar,
             );
             $req->fromJsonString(json_encode($params));
-
-            // 返回的resp是一个RegisterUserResponse的实例，与请求对象对应
-            $resp = $client->RegisterUser($req);
+            $resp = $this->client->RegisterUser($req);
 
             // 输出json格式的字符串回包
 //            print_r($resp->toJsonString());
