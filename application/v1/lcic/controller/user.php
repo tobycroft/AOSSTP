3aa6c24b44136dfaeed3576d1010d241817dfc53
火线兔 <silverhawk@foxmail.com>
2023-03-08 17:09:54 +0800

    TuuzMac

--- application/v1/lcic/controller/user.php
+++ application/v1/lcic/controller/user.php
@@ -11,6 +11,7 @@ use TencentCloud\Common\Profile\ClientProfile;
 use TencentCloud\Common\Profile\HttpProfile;
 use TencentCloud\Lcic\V20220817\LcicClient;
 use TencentCloud\Lcic\V20220817\Models\BatchRegisterRequest;
+use TencentCloud\Lcic\V20220817\Models\ModifyUserProfileRequest;
 use TencentCloud\Lcic\V20220817\Models\RegisterUserRequest;
 
 
@@ -25,6 +26,10 @@ class user extends create
 
     protected mixed $wechat;
 
+    protected mixed $cred;
+    protected mixed $httpProfile;
+    protected mixed $clientProfile;
+
 
     public function initialize()
     {
@@ -37,6 +42,18 @@ class user extends create
         $this->secretid = $this->wechat['secretid'];
         $this->secretkey = $this->wechat['secretkey'];
         $this->access_token = $this->wechat['access_token'];
+        if (!$this->cred) {
+            $this->cred = new Credential($this->secretid, $this->secretkey);
+        }
+        if (!$this->httpProfile) {
+            $this->httpProfile = new HttpProfile();
+            $this->httpProfile->setEndpoint('lcic.tencentcloudapi.com');
+        }
+        if (!$this->clientProfile) {
+            $clientProfile = new ClientProfile();
+            $clientProfile->setHttpProfile($this->httpProfile);
+        }
+
     }
 
     public function create()
@@ -49,16 +66,13 @@ class user extends create
             // 实例化一个认证对象，入参需要传入腾讯云账户 SecretId 和 SecretKey，此处还需注意密钥对的保密
             // 代码泄露可能会导致 SecretId 和 SecretKey 泄露，并威胁账号下所有资源的安全性。以下代码示例仅供参考，建议采用更安全的方式来使用密钥，请参见：https://cloud.tencent.com/document/product/1278/85305
             // 密钥可前往官网控制台 https://console.cloud.tencent.com/cam/capi 进行获取
-            $cred = new Credential($this->secretid, $this->secretkey);
             // 实例化一个http选项，可选的，没有特殊需求可以跳过
-            $httpProfile = new HttpProfile();
-            $httpProfile->setEndpoint('lcic.tencentcloudapi.com');
+
 
             // 实例化一个client选项，可选的，没有特殊需求可以跳过
-            $clientProfile = new ClientProfile();
-            $clientProfile->setHttpProfile($httpProfile);
+
             // 实例化要请求产品的client对象,clientProfile是可选的
-            $client = new LcicClient($cred, '', $clientProfile);
+            $client = new LcicClient($this->cred, '', $this->clientProfile);
 
             // 实例化一个请求对象,每个接口都会对应一个request对象
             $req = new RegisterUserRequest();
