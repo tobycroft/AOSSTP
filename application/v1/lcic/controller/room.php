2e21dd6a99c960a836d91a605236e3c98fabb4b6
火线兔 <silverhawk@foxmail.com>
2023-03-16 12:47:01 +0800

    TuuzMac

--- application/v1/lcic/controller/room.php
+++ application/v1/lcic/controller/room.php
@@ -30,7 +30,7 @@ class room extends user
         $TeacherId = Input::PostInt("TeacherId");
         $StartTime = Input::PostInt("StartTime");
         $EndTime = Input::PostInt("EndTime");
-        $user = LcicUserModel::where('project', $this->token)->whereOr(['OriginId' => $TeacherId, "UserId" => $TeacherId])->findOrEmpty();
+        $user = LcicUserModel::where('project', $this->token)->where(['OriginId' => $TeacherId])->findOrEmpty();
         echo $user->getLastSql();
         var_dump($user);
         exit();
