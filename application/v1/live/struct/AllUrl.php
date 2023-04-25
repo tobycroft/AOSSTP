6e54784dc08784908b2edba0f3a2235384bebf70
火线兔 <silverhawk@foxmail.com>
2023-04-25 13:24:54 +0800

    TuuzMac

--- application/v1/live/struct/AllUrl.php
+++ application/v1/live/struct/AllUrl.php
@@ -40,8 +40,8 @@ class AllUrl
         $this->srt = 'srt://' . $this->domain . ':9000?streamid=#!::h=' . $this->domain . ',r=live/' . $streamName . ',txSecret=' . $txPushSecret . ',txTime=' . $txTime;
         $this->webrtc = 'webrtc://' . $this->domain . '/live/' . $this->stream_code;
         if (!empty($moban)) $streamName .= '_' . $moban;
-        $this->play_flv = '//' . $this->play_domain . '/live/' . $streamName . '.flv?' . $play_str;
-        $this->play_hls = '//' . $this->play_domain . '/live/' . $streamName . '.m3u8?' . $play_str;
+        $this->play_flv = '' . $this->play_domain . '/live/' . $streamName . '.flv?' . $play_str;
+        $this->play_hls = '' . $this->play_domain . '/live/' . $streamName . '.m3u8?' . $play_str;
         $this->play_rtmp = 'rtmp://' . $this->play_domain . '/live/' . $streamName . '.m3u8?' . $play_str;
     }
 }
