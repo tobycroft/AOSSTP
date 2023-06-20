23e174cb6b5a1cf10597e467cadda2ea931d6fe2
火线兔 <silverhawk@foxmail.com>
2023-06-20 14:25:31 +0800

    TuuzMac

--- application/v1/excel/controller/index.php
+++ application/v1/excel/controller/index.php
@@ -8,6 +8,8 @@ use app\v1\file\model\AttachmentModel;
 use app\v1\project\model\ProjectModel;
 use BaseController\CommonController;
 use PhpOffice\PhpSpreadsheet\IOFactory;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 use Ret;
 use think\facade\Validate;
 use think\Request;
@@ -31,6 +33,38 @@ class index extends CommonController
         }
         $this->proc = OssSelectionAction::App_find_byProc($this->proc);
 
+    }
+
+    public function create()
+    {
+        $data = \Input::Post("data");
+
+        # 实例化 Spreadsheet 对象
+        $spreadsheet = new Spreadsheet();
+
+        # 获取活动工作薄
+        $sheet = $spreadsheet->getActiveSheet();
+
+        # 获取单元格
+        $cellA = $sheet->getCell('A1');
+
+        # 设置单元格值
+        $cellA->setValue('欧阳克');
+
+        # 获取单元格
+        $cellB = $sheet->getCellByColumnAndRow(1, 2);
+
+        # 设置单元格值
+        $cellB->setValue('黄蓉');
+
+        # 获取设置单元格，链式操作
+        $sheet->getCell('A3')->setValue('郭靖');
+        $sheet->getCellByColumnAndRow(1, 4)->setValue('杨康');
+
+        # Xlsx类 将电子表格保存到文件
+
+        $writer = new Xlsx($spreadsheet);
+        $writer->save('./upload/excel/' . $this->token . '/tempfile/' . time() . ".xlsx");
     }
 
     public function index(Request $request)
