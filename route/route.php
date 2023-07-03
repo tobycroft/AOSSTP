bc28d5225ed2ea0a894c9af73387efbaf91bd2d9
火线兔 <silverhawk@foxmail.com>
2023-07-03 13:06:40 +0800

    TuuzMac

--- route/route.php
+++ route/route.php
@@ -21,7 +21,7 @@
     header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
     header('Access-Control-Allow-Headers: *');
     if (\think\facade\Request::isOptions()) {
-        return;
+        return false;
     }
 });
 
