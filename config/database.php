fcdfe45851a6b7da68fc1bfdcc080e846c60c212
火线兔 <silverhawk@foxmail.com>
2023-07-11 17:00:55 +0800

    TuuzMac

--- config/database.php
+++ config/database.php
@@ -17,27 +17,27 @@ return [
     // 数据库名
     'database'        => env("database.database"),
     // 用户名
-    'username'        => env("database.username"),
+    'username' => env("database.username"),
     // 密码
-    'password'        => env("database.password"),
+    'password' => env("database.password"),
     // 端口
-    'hostport'        => env("database.hostport"),
+    'hostport' => env("database.hostport"),
     // 连接dsn
-    'dsn'             => '',
+    'dsn' => '',
     // 数据库连接参数
-    'params'          => [],
+    'params' => [],
     // 数据库编码默认采用utf8
-    'charset'         => 'utf8',
+    'charset' => 'utf8mb4',
     // 数据库表前缀
-    'prefix'          => '',
+    'prefix' => '',
     // 数据库调试模式
-    'debug'           => true,
+    'debug' => true,
     // 数据库部署方式:0 集中式(单一服务器),1 分布式(主从服务器)
-    'deploy'          => 0,
+    'deploy' => 0,
     // 数据库读写是否分离 主从式有效
-    'rw_separate'     => false,
+    'rw_separate' => false,
     // 读写分离后 主服务器数量
-    'master_num'      => 1,
+    'master_num' => 1,
     // 指定从服务器序号
     'slave_no'        => '',
     // 自动读取主库数据
