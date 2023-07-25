6b1678097e973b43b1246969f961ee0c99265e20
火线兔 <silverhawk@foxmail.com>
2023-07-25 15:07:19 +0800

    TuuzMac

--- extend/WlwxSMS/Send.php
+++ extend/WlwxSMS/Send.php
@@ -56,12 +56,12 @@ class Send
         $response = curl_exec($ch);  //>运行curl
         if ($response === false) {
             if (curl_errno($ch) == CURLE_OPERATION_TIMEDOUT) {
-                throw new Exception('政务云短信平台超时');
+                throw new Exception('云短信平台超时');
             }
         }
         if (empty($response)) {
             if (curl_errno($ch) == CURLE_OPERATION_TIMEDOUT) {
-                throw new Exception('政务云短信平台无返回');
+                throw new Exception('短信平台无返回');
             }
         }
         curl_close($ch);        //>关闭curl
